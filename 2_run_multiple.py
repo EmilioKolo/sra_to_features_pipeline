55de7e6d0e063d1b6df8e2356a2fab097f844fbb
EmilioKolo <emilio_193@hotmail.com>
2025-06-06 16:37:44 -0300

    Update 2_run_multiple.py
    Make it run with the sra lists provided. Include installation scripts and venv creation.

--- 2_run_multiple.py
+++ 2_run_multiple.py
@@ -4,13 +4,31 @@ import os
 import sys
 
 
+venv_name = 'sra_to_vcf'
+
 if len(sys.argv) > 1:
     sra_file = sys.argv[1]
     print(f"The provided file with SRA IDs is: {sra_file}")
 else:
-    print("No filename provided.")
-    raise ValueError
+    sra_file = 'sra_table_selected.txt'
+    print(f'No filename provided. Running with default file "{sra_file}".')
 
+# Create a virtual environment
+if not os.path.exists(venv_name):
+    os.system(f'python3 -m venv ~/{venv_name}')
+    print("Virtual environment created.")
+
+# Activate the virtual environment
+activate_script = f'~/{venv_name}/bin/activate'
+os.system(f'source {activate_script}')
+
+# Make install script executable
+l = 'chmod +x ./0_install.sh'
+os.system(l)
+
+# Run install
+l = './0_install.sh'
+os.system(l)
 
 # Initialize dict-list of sras
 sra_data = {}
