e770fa8901ceef715039ffe1b7f2479c64721a7e
EmilioKolo <emilio_193@hotmail.com>
2025-06-30 12:07:01 -0300

    Update script.sh
    Make it work in the possibility of a windows edited sra table.

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Remove Windows carriage returns
+sed -i 's/\r//' sra_table_test.txt
+# Run code with parallel
 cat sra_table_selected.txt | parallel -j2 'docker run -v ../output/:/content/data/output -e HOST_UID=$(id -u) -e HOST_GID=$(id -g) features-pipeline {} /content/data/output'
