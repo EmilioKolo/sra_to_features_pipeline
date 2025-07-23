bbcd557feaa05ed4b7cbb19eb5bb95de7c960083
EmilioKolo <emilio_193@hotmail.com>
2025-07-23 06:49:08 -0300

    Rework setup folders
    Remove tmp
    
    Move bin and logs to base dir instead of data dir
    
    Change corresponding variables in config.ini

--- setup.sh
+++ setup.sh
@@ -58,14 +58,12 @@ mkdir -p "$BASE_DIR"
 # Create data, install, logs, bin and tmp directories
 DATA_DIR="$BASE_DIR/data"
 INSTALL_DIR="$BASE_DIR/install"
-LOGS_DIR="$DATA_DIR/logs"
-BIN_DIR="$DATA_DIR/bin"
-TMP_DIR="$DATA_DIR/tmp"
+LOGS_DIR="$BASE_DIR/logs"
+BIN_DIR="$BASE_DIR/bin"
 mkdir -p "$DATA_DIR"
 mkdir -p "$INSTALL_DIR"
 mkdir -p "$LOGS_DIR"
 mkdir -p "$BIN_DIR"
-mkdir -p "$TMP_DIR"
 # Define snpEff variables
 snpeff_jar="$SNPEFF_DIR/snpEff/snpEff.jar"
 # Add the bin directory to PATH
