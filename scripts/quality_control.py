2f16a8d431b681e62f96a7632f459022b9a2c433
EmilioKolo <emilio_193@hotmail.com>
2025-07-28 13:48:22 -0300

    Update quality_control.py
    Add initial description.
    
    Add logging.
    
    Add coverage functions (not yet implemented into perform_checks function).

--- scripts/quality_control.py
+++ scripts/quality_control.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 
+"""
+Functions used for quality control and quality checks of the output files.
+"""
+
+from scripts.log_scripts import *
 import os
 
 
@@ -30,6 +35,8 @@ def perform_checks(dict_var:dict) -> None:
             bam_file=dict_var['sorted_bam'],
             kraken_db=dict_var['kraken_db'],
             output_dir=kraken_out,
+            log_file=dict_var['log_print'],
+            log_scr=dict_var['log_scripts'],
             threads=dict_var['THREADS']
         )
     except Exception as e:
@@ -41,7 +48,11 @@ def perform_checks(dict_var:dict) -> None:
         # Go through full paths of all fastq files
         for fastq_file in dict_var['l_fastq_full']:
             # Run FastQC on each file
-            run_fastqc(fastq_file, fastqc_out)
+            run_fastqc(
+                fastq_file,
+                fastqc_out,
+                log_scr=dict_var['log_scripts']
+            )
     except Exception as e:
         print(f"Error analyzing fastq files: {e}")
         print("Skipping fastq file analysis.")
@@ -55,6 +66,8 @@ def check_kraken(
         bam_file:str,
         kraken_db:str,
         output_dir:str,
+        log_file:str,
+        log_scr:str,
         threads:int=1
     ) -> None:
     """
@@ -72,24 +85,43 @@ def check_kraken(
     kraken_report = os.path.join(output_dir, sra_id+'_kraken2_report.txt')
 
     # Extract unaligned reads
-    print('Extracting unaligned reads from BAM file...')
+    log_print(
+        'Extracting unaligned reads from BAM file...',
+        level='info',
+        log_file=log_file
+    )
     l = f'samtools view -f 4 -b {bam_file} -o {unaligned_bam}'
+    log_code(l, log_file=log_scr)
     os.system(l)
-    print('Converting unaligned BAM to FASTQ...')
+    log_print(
+        'Converting unaligned BAM to FASTQ...',
+        level='info',
+        log_file=log_file
+    )
     l = f'samtools fastq {unaligned_bam} -o {unaligned_fastq}'
+    log_code(l, log_file=log_scr)
     os.system(l)
 
-    print('Running Kraken2 classification...')
+    log_print(
+        'Running Kraken2 classification...',
+        level='info',
+        log_file=log_file
+    )
     # Classify with Kraken2
     l = f'kraken2 --db {kraken_db} --threads {threads}'
     l += f' --output {kraken_output} --report {kraken_report}'
     l += f' {unaligned_fastq}'
+    log_code(l, log_file=log_scr)
     os.system(l)
 
     # Visualize output data
     p = 'Kraken2 classification complete. Results in:'
     p += f'\n * {kraken_output}\n * {kraken_report}'
-    print(p)
+    log_print(
+        p,
+        level='info',
+        log_file=log_file
+    )
 
     # Clean up intermediate files
     os.remove(unaligned_bam)
@@ -98,7 +130,8 @@ def check_kraken(
 
 def run_fastqc(
         fastq_file:str,
-        output_dir:str
+        output_dir:str,
+        log_scr:str
     ) -> None:
     """
     Runs FastQC on a fastq file.
@@ -115,12 +148,14 @@ def run_fastqc(
     # Define fastq file to analyze
     l += f' {fastq_file}'
     # Run the script
+    log_code(l, log_file=log_scr)
     os.system(l)
     return None
 
 def run_fastqc_folder(
         fastq_folder:str,
         output_dir:str,
+        log_scr:str,
         threads:int=1
     ) -> None:
     """
@@ -140,5 +175,76 @@ def run_fastqc_folder(
     # Define fastq folder to analyze
     l += f' {fastq_folder}/*'
     # Run the script
+    log_code(l, log_file=log_scr)
+    os.system(l)
+    return None
+
+def run_bedtools_coverage(
+        sra_id:str,
+        bam_sorted:str,
+        output_dir:str,
+        log_scr:str,
+        region_file:str=''
+    ) -> None:
+    """
+    Runs bedtools coverage for a list of regions (optional).
+    """
+    # Make the output directory if it does not exist
+    try:
+        os.mkdir(output_dir)
+    except FileExistsError:
+        pass
+    # Define output file
+    output_file = os.path.join(
+        output_dir,
+        sra_id+'_bedtools_coverage_hist.txt'
+    )
+    # Initialize the script to run
+    l = 'bedtools coverage'
+    # Add region file if provided
+    if region_file!='':
+        l += f' -a {region_file}'
+    # Add bam file
+    l += f' -b {bam_sorted}'
+    # Define as histogram
+    l += ' -hist'
+    # Define output
+    l += f' > {output_file}'
+    # Run the script
+    log_code(l, log_file=log_scr)
+    os.system(l)
+    return None
+
+def run_samtools_depth(
+        sra_id:str,
+        bam_sorted:str,
+        output_dir:str,
+        log_scr:str,
+        region_file:str=''
+    ) -> None:
+    """
+    Runs samtools depth for a list of regions (optional).
+    """
+    # Make the output directory if it does not exist
+    try:
+        os.mkdir(output_dir)
+    except FileExistsError:
+        pass
+    # Define output file
+    output_file = os.path.join(
+        output_dir,
+        sra_id+'_samtools_depth.txt'
+    )
+    # Initialize the script to run
+    l = 'samtools depth'
+    # Add region file if provided
+    if region_file!='':
+        l += f' -b {region_file}'
+    # Add bam file
+    l += f' {bam_sorted}'
+    # Define output
+    l += f' > {output_file}'
+    # Run the script
+    log_code(l, log_file=log_scr)
     os.system(l)
     return None
